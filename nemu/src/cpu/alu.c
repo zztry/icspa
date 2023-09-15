19864d3004562767c9da02503184bcd53d22057e
tracer-pa-public <tracer@njuics.org>
2023-09-15 23:20:45 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:20:45 up 60 days, 10:07,  1 user,  load average: 2.75, 2.59, 2.61 > unitime > 8267dfd436f34e250574a229d6fbb4eb6e46f5

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -5,10 +5,16 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = dest + src;
+	
+	set_CF_add(res, src, data_size);//unsigned arithmetic overflow
+	set_PF(res);//
+	set_ZF(res ,data_size);
+	set_SF(res, data_size);
+	set_OF_add(res, src, dest, data_size);
+	
+	return res & (0xFFFFFFFF >> (32- data_size));
 #endif
 }
 
@@ -205,3 +211,89 @@ uint32_t alu_sal(uint32_t src, uint32_t dest, size_t data_size)
 	return 0;
 #endif
 }
+
+
+//CF contains information relevant to unsigned integers
+void set_CF_add(uint32_t result,uint32_t src, size_t data_size)
+{
+    result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);//保留data_size位，符号扩展
+    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    cpu.eflags.CF = (result<src)
+    
+}
+
+void set_PF(uint32_t result)
+{
+    res[8];
+    res[0] = result & 0x00000001;
+    res[1] = (result & 0x00000002)>>1;
+    res[2] = (result & 0x00000004)>>2;
+    res[3] = (result & 0x00000008)>>3;
+    res[4] = (result & 0x00000010)>>4;
+    res[5] = (result & 0x00000020)>>5;
+    res[6] = (result & 0x00000040)>>6;
+    res[7] = (result & 0x00000080)>>7;
+    
+    int even = 0;
+    for(int i = 0;i<8;i++)
+    {
+        if(res[i]==1){
+            even+=1;
+        }
+    }
+    if(even%2==0)
+    {
+        cpu.eflags.PF=1;
+    }
+    else{
+        cpu.eflags.PF=0;
+    }
+}
+
+void set_ZF(uint32_t result, size_t data_size) 
+{
+    result = result & (0xFFFFFFFF >> (32 - data_size));//保留data_size位
+	cpu.eflags.ZF = (result == 0);
+}
+
+// SF and OF contain information relevant to signed integers
+void set_SF(res, data_size);
+{
+    result = sign_ext(result & ( 0xFFFFFFFF >>(32 - data_size) ));//符号扩展
+    cpu.eflags.SF = signed(result)//此函数取32位的最高位
+}
+
+void set_OF_add(res, src, dest, data_size)
+{
+    //先进行符号扩展
+    if(data_size==8){
+        result = sign_ext(result & 0xFF, 8); 
+		src = sign_ext(src & 0xFF, 8); 
+		dest = sign_ext(dest & 0xFF, 8); 
+
+    }
+    else if(data_size ==16){
+        result = sign_ext(result & 0xFFFF, 16); 
+		src = sign_ext(src & 0xFFFF, 16); 
+		dest = sign_ext(dest & 0xFFFF, 16); 
+    }
+    
+    //如果一正一负必为0，符号相同则判断
+    if(signed(dest)==signed(src))
+    {
+        if(signed(res)!=signed(src))
+        {
+            cpu.eflags.OF = 1;
+        }
+        else{
+            cpu.eflags.OF = 0;
+        }
+    }
+    else{
+        cpu.eflags.OF = 0;
+    }
+}
+
+
+
+
