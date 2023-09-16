3a50aeddc4956f99fb7e0bbf61e43df81bdd92d4
tracer-pa-public <tracer@njuics.org>
2023-09-16 21:24:07 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:24:07 up 61 days,  8:10,  1 user,  load average: 3.57, 3.11, 2.91 > unitime > c0b3d29f8c0543279bf1c9a2fa1f437ef67d4248

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -63,7 +63,7 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = 0;
 	res = dest - src - cpu.eflags.CF;
 	
-	set_CF_sub(res, dest, data_size);
+	set_CF_sbb(res, src ,dest, data_size);
 	set_PF(res);
 	set_ZF(res ,data_size);
 	set_SF(res, data_size);
@@ -266,6 +266,22 @@ void set_CF_sub(uint32_t result, uint32_t dest,size_t data_size)
     result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
     dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
     cpu.eflags.CF = (result>dest);
+}
+
+void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size)
+{
+    //根据adc修改
+    uint32_t res1 = dest - src;
+    res1 = sign_ext(res1 & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    if(res1>src)
+    {
+        cpu.eflags.CF = (res1>src);
+    }
+    else{
+        result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+        cpu.eflags.CF = (result>res1);
+    }
 }
 
 void set_PF(uint32_t result)
