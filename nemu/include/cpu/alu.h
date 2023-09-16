aefbef1f667f232cfd8bf3be3ebc8df835df4604
tracer-pa-public <tracer@njuics.org>
2023-09-16 21:08:08 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:08:08 up 61 days,  7:54,  1 user,  load average: 2.93, 3.06, 2.87 > unitime 1694869688463 > 5fcf2cbf5eec743bb1cadebdfa7df591a5c08cfa

--- nemu/include/cpu/alu.h
+++ nemu/include/cpu/alu.h
@@ -78,6 +78,6 @@ void set_OF_add(uint32_t res,uint32_t src,uint32_t dest,size_t data_size);
 void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest, size_t data_size);
 void set_OF_adc(uint32_t res,uint32_t src,uint32_t dest,uint32_t CF,size_t data_size);
 void set_CF_sub(uint32_t result, uint32_t dest,size_t data_size);
-void set_OF_sub(uint32_t res,uint32_t src,uint32_t dest,size_t data_size)
+void set_OF_sub(uint32_t res,uint32_t src,uint32_t dest,size_t data_size);
 
 #endif
