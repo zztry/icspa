368842e3d9ee2189b6076a0cc727d8cca30a5246
tracer-pa-public <tracer@njuics.org>
2023-09-17 17:34:26 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:34:26 up 62 days,  4:21,  1 user,  load average: 2.88, 2.87, 2.71 > unitime 1694943266016 > 77fe3f3813c634ad6c4b1f5b10e2c90513840c86

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -311,7 +311,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	set_SF(res, data_size);
 	
 	
-	return res & (0xFFFFFFFF >> (32 - data_size);
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
