bdb40351f215627ce00a1399c21229dfaa925c14
tracer-pa-public <tracer@njuics.org>
2023-09-17 20:23:46 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:23:46 up 62 days,  7:10,  1 user,  load average: 2.94, 2.99, 2.86 > unitime > 2e9f4833bc19bb4557dcbccf95efad942da66269

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -78,8 +78,8 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_mul(src, dest, data_size);
 #else
-    src = src & (0xFFFFFFFFFFFFFFFF >> (32- data_size));
-    dest = dest & (0xFFFFFFFFFFFFFFFF >> (32- data_size));
+    src = src & (0xFFFFFFFF >> (32- data_size));
+    dest = dest & (0xFFFFFFFF >> (32- data_size));
 	uint64_t res = 0;
 	res = dest * src;
 	res = res & (0xFFFFFFFFFFFFFFFF >> (64- data_size*2));
