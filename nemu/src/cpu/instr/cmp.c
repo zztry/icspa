5879421d751a766d33f76c600d28fc0353b5b007
tracer-pa-public <tracer@njuics.org>
2023-10-15 20:40:42 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:40:42 up 90 days,  7:27,  1 user,  load average: 2.09, 2.44, 2.51 > unitime 1697373642434 > 587a06c354147d502e256b7d9cdf037090460b4f

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -2,3 +2,42 @@
 /*
 Put the implementations of `cmp' instructions here.
 */
+
+make_instr_func(cmp_i2rm_bv) //8->16/32
+{
+    OPERAND rm,imm;
+    
+    rm.data_size = data_size; // data_size是个全局变量，表示操作数的比特长度
+    int len = 1;                          // opcode 长度1字节
+    len += modrm_rm(eip + 1, &rm); // 读ModR/M字节，rm的type和addr会被填写
+    
+    imm.type = OPR_IMM;        // 填入立即数类型
+    imm.addr = eip + len;           // 找到立即数的地址
+    imm.data_size = data_size;  
+    
+    operand_read(&rm);
+    operand_read(&imm);
+    int32_t result = sign_ext(rm.val, data_size)-sign_ext(imm.val, 8) ;
+    
+    if(result==0)
+    {
+        cpu.eflags.ZF=1;
+    }
+    else
+    {
+        cpu.eflags.ZF=0;
+    }
+    
+    if(result>=0)
+    {
+        cpu.eflags.CF=0;
+    }
+    else
+    {
+        cpu.eflags.CF=1;
+    }
+    
+    return len + data_size / 8;    // opcode长度 + ModR/M字节扫描长度 + 立即数长度
+    
+
+}
