c1ace1afe2d74fc53d4563821570de443f9db6fa
tracer-pa-public <tracer@njuics.org>
2023-10-20 11:37:46 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:37:46 up 94 days, 22:24,  1 user,  load average: 2.16, 2.33, 2.44 > unitime 1697773065899 > dc1ad19dbfa527b1657e61af503a7fa3e9dfd4ba

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -5,5 +5,6 @@ Put the declarations of `cmp' instructions here.
 */
 
 make_instr_func(cmp_i2rm_bv);
+make_instr_func(cmp_i2r_b);
 
 #endif
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -41,3 +41,42 @@ make_instr_func(cmp_i2rm_bv) //8->16/32
     
 
 }
+
+
+make_instr_func(cmp_i2r_b)//8 i->al;
+{
+    OPERAND r,imm;
+    
+    r.data_size = data_size; // data_size是个全局变量，表示操作数的比特长度
+    int len = 1;                          // opcode 长度1字节
+    r.type = OPR_REG; 
+    r.addr = 0;//al寄存器
+    
+    imm.type = OPR_IMM;        // 填入立即数类型
+    imm.addr = eip + len;           // 找到立即数的地址
+    imm.data_size = 8;  //8字节
+    
+    operand_read(&r);
+    operand_read(&imm);
+    int32_t result = sign_ext(r.val, data_size)-sign_ext(imm.val, 8) ;
+    
+    if(result==0)
+    {
+        cpu.eflags.ZF=1;
+    }
+    else
+    {
+        cpu.eflags.ZF=0;
+    }
+    
+    if(result>=0)
+    {
+        cpu.eflags.CF=0;
+    }
+    else
+    {
+        cpu.eflags.CF=1;
+    }
+    
+    return len + 1;    // opcode长度 + ModR/M字节扫描长度 + 立即数长度,这里为1
+}
