966169d9051377cb5c4b2cc9ac2449a33a92b52f
tracer-pa-public <tracer@njuics.org>
2023-10-20 11:20:11 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:20:11 up 94 days, 22:06,  1 user,  load average: 2.53, 2.37, 2.40 > unitime 1697772011809 > e50a8d1fc0131b9564c3c0b8443c532dd1ef9d

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,8 +2,33 @@
 /*
 Put the implementations of `push' instructions here.
 */
-/*
-make_instr_func(push_r_l)
+
+make_instr_func(push_r_l)//32
 {
+    OPERAND r1,r2,m;        // 创建源操作数和目的操作数（ESP）局部变量
+
     
-}*/
+    
+    r1.data_size = 8;        // 配置源操作数大小
+    r1.type = OPR_REG;       // 配置源操作数类型，通用寄存器
+    r1.addr = opcode & 0x7;  // 配置源操作数地址，即寄存器地址
+    
+    r2.data_size = 8;    //ESP    
+    r2.type = OPR_REG;       
+    r2.addr = &cpu.gpr[4];  
+    
+    operand_read(&r2);
+    r2.val = r2.val-4;      // 将源操作数的值赋给栈
+    operand_write(&r2);    // esp-4
+    
+    m.data_size = 32; // data_size是个全局变量，表示操作数的比特长度
+    m.type = OPR_MEM;
+    m.addr = r2.val;
+    
+    operand_read(&r1);   // 读通用寄存器内的值
+    operand_read(&m);
+    m.val = r1.val;      // 将源操作数的值赋给栈
+    operand_write(&m);    
+    
+    return 2;             // 返回指令长度
+}
