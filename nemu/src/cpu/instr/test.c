59923d90aff44e0e49830ce8a20d0e049a611bad
tracer-pa-public <tracer@njuics.org>
2023-10-22 00:18:13 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:18:13 up 96 days, 11:04,  1 user,  load average: 2.35, 2.61, 2.74 > unitime 1697905093734 > 8d115ae9af704a24503a9106aa969e0fd3e0

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -13,7 +13,7 @@ static void instr_execute_2op()
 	//opr_dest.val = sign_ext(opr_dest.val,opr_dest.data_size);
 	//opr_src.val = sign_ext(opr_src.val,opr_src.data_size); 
 	
-	alu_and(opr_src.val,opr_dest.val,data_size);
+	//alu_and(opr_src.val,opr_dest.val,data_size);
 	//????
 	
 	/*
