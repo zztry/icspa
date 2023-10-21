991e9cbefe9df920960bb7138bb4cfc8c4301c50
tracer-pa-public <tracer@njuics.org>
2023-10-21 22:37:07 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:37:07 up 96 days,  9:23,  1 user,  load average: 3.15, 2.79, 2.69 > unitime 1697899027544 > 537cce3c4a2f9af41c74546975044c4a40b3224b

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -1,23 +1,23 @@
 #ifndef __INSTR_JCC_H__
 #define __INSTR_JCC_H__
 
-make_instr_func(je_short_);
-make_instr_func(ja_short_);
-make_instr_func(jae_short_);
-make_instr_func(jb_short_);
-make_instr_func(jecxz_short_);
-make_instr_func(jg_short_);
-make_instr_func(jge_short_);
-make_instr_func(jl_short_);
-make_instr_func(jle_short_);
-make_instr_func(jna_short_);
-make_instr_func(jne_short_);
-make_instr_func(jno_short_);
-make_instr_func(jnp_short_);
-make_instr_func(jns_short_);
-make_instr_func(jo_short_);
-make_instr_func(jp_short_);
-make_instr_func(js_short_);
+make_instr_func(je_short_);         //74 ok
+make_instr_func(ja_short_);         //77
+make_instr_func(jae_short_);        //73
+make_instr_func(jb_short_);         //72
+make_instr_func(jecxz_short_);      //e3
+make_instr_func(jg_short_);         //7f o
+make_instr_func(jge_short_);        //7d
+make_instr_func(jl_short_);         //7c
+make_instr_func(jle_short_);        //7e
+make_instr_func(jna_short_);        //76
+make_instr_func(jne_short_);        //75
+make_instr_func(jno_short_);        //71
+make_instr_func(jnp_short_);        //7b
+make_instr_func(jns_short_);        //79
+make_instr_func(jo_short_);         //70
+make_instr_func(jp_short_);         //7a
+make_instr_func(js_short_);         //78
 
 make_instr_func(je_near);
 make_instr_func(ja_near);
