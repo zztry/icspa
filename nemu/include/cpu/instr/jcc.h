8b30c7b802a4ca33a3158dd8bc3c0bac8af3692a
tracer-pa-public <tracer@njuics.org>
2023-10-21 22:42:10 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:42:10 up 96 days,  9:28,  1 user,  load average: 2.10, 2.44, 2.57 > unitime 1697899330542 > 431c6e23c14786cc91980abb01135c82fa260ab

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -6,10 +6,10 @@ make_instr_func(ja_short_);         //77
 make_instr_func(jae_short_);        //73
 make_instr_func(jb_short_);         //72
 make_instr_func(jecxz_short_);      //e3
-make_instr_func(jg_short_);         //7f o
+make_instr_func(jg_short_);         //7f ok
 make_instr_func(jge_short_);        //7d
 make_instr_func(jl_short_);         //7c
-make_instr_func(jle_short_);        //7e
+make_instr_func(jle_short_);        //7e ok
 make_instr_func(jna_short_);        //76
 make_instr_func(jne_short_);        //75
 make_instr_func(jno_short_);        //71
