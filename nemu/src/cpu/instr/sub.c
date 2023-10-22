577cb4df9b81b287a5e1630b1cfd4ee8f8f7606d
tracer-pa-public <tracer@njuics.org>
2023-10-22 15:52:06 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:52:06 up 97 days,  2:38,  1 user,  load average: 2.31, 2.55, 2.65 > unitime 1697961126666 > 7f0c2074966d52d61de84966b4e1a06ad47b611

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -2,3 +2,26 @@
 /*
 Put the implementations of `sub' instructions here.
 */
+//用宏实现make_instr_impl_2op(inst_name, src_type, dest_type, suffix)  
+
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	
+	if(opr_src.data_size==8 && (opr_dest.data_size>=16))
+	{
+	    opr_dest.val=alu_sub(opr_dest.val,sign_ext(opr_src.val,opr_src.data_size),data_size);
+	}
+	else
+	{
+	    opr_dest.val=alu_sub(opr_dest.val,opr_src.val,data_size);
+	}
+	operand_write(&opr_dest);
+	
+}
+
+//83 /5 ib SUB r/m16,imm8 2/7 Subtract sign-extended immediate byte from r/m word
+//83 /5 ib SUB r/m32,imm8 2/7 Subtract sign-extended immediate byte from r/m dword
+//  i rm v-16/32            sub_i2rm_v
+make_instr_impl_2op(sub,i,rm,v);
