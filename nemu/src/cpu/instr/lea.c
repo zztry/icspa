ace578cc8e6c1f6622de84e75dcbbd558e48ab40
tracer-pa-public <tracer@njuics.org>
2023-10-23 00:19:41 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:19:41 up 97 days, 11:06,  1 user,  load average: 2.79, 2.67, 2.61 > unitime 1697991581788 > ab4f13b4486f0aaf392a51b35f5fadb34c9e037

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -21,6 +21,7 @@ make_instr_func(lea_rm2r_v)//m地址 源操作数rm 目的操作数r
         opr_dest.val = opr_src.addr;//这里没处理16->32的0扩展
     }
     operand_write(&opr_dest);
+    return len;
 }
 //8D /r LEA r16,m 2 Store effective address for m in register r16
 //8D /r LEA r32,m 2 Store effective address for m in register r32
