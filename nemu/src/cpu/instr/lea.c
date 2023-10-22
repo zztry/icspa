5b37106d7dc8a3ab86402360d2491b3586d0b045
tracer-pa-public <tracer@njuics.org>
2023-10-23 00:18:06 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:18:06 up 97 days, 11:04,  1 user,  load average: 2.26, 2.54, 2.57 > unitime 1697991486065 > 1aff1429460ce6911fcec6975fc8f2bdfd3e64e9

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -2,3 +2,27 @@
 /*
 Put the implementations of `lea' instructions here.
 */
+
+
+make_instr_func(lea_rm2r_v)//m地址 源操作数rm 目的操作数r
+{
+    int len = 1;
+    opr_src.data_size = data_size;
+    opr_dest.data_size = data_size;
+    len += modrm_r_rm(eip + 1, &opr_dest, &opr_src);
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    if(opr_dest.data_size==16)
+    {
+        opr_dest.val = (0xffffffff>>16)opr_src.addr;
+    }
+    else if(opr_dest.data_size==32)
+    {
+        opr_dest.val = opr_src.addr;//这里没处理16->32的0扩展
+    }
+    operand_write(&opr_dest);
+}
+//8D /r LEA r16,m 2 Store effective address for m in register r16
+//8D /r LEA r32,m 2 Store effective address for m in register r32
+//8D /r LEA r16,m 2 Store effective address for m in register r16
+//8D /r LEA r32,m 2 Store effective address for m in register r32
