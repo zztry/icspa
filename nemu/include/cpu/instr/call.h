ea93b22de3f994b7c674085f356553c78deb84c3
tracer-pa-public <tracer@njuics.org>
2023-10-25 15:56:12 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:56:12 up 100 days,  2:42,  2 users,  load average: 2.43, 2.54, 2.68 > unitime 1698220571862 > 5faf55f690eeed8d60b137e8e6b85b7de0f70004

--- nemu/include/cpu/instr/call.h
+++ nemu/include/cpu/instr/call.h
@@ -1,5 +1,6 @@
 #ifndef __INSTR_CALL_H__
 #define __INSTR_CALL_H__
+
 /*
 Put the declarations of `call' instructions here.
 */
--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,3 +2,9 @@
 /*
 Put the implementations of `call' instructions here.
 */
+
+//E8 cw CALL rel16 7+m Call near, displacement relative to next instruction
+make_instr_func(call_near)
+{
+    
+}
