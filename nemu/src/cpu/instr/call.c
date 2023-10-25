f9a1742d3f25a911b31579ef2983afc95813be95
tracer-pa-public <tracer@njuics.org>
2023-10-25 16:16:21 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:16:21 up 100 days,  3:02,  2 users,  load average: 3.09, 2.77, 2.59 > unitime 1698221781151 > b492708b2ad54db54d6111c9639a94ef2f978df9

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -4,7 +4,8 @@ Put the implementations of `call' instructions here.
 */
 
 //E8 cw CALL rel16 7+m Call near, displacement relative to next instruction
+/*
 make_instr_func(call_near)
 {
     
-}
+}*/
