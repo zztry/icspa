cabf272a04dd4ad9da28d266a14f07d6ddd31752
tracer-pa-public <tracer@njuics.org>
2023-10-26 23:26:52 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:26:52 up 101 days, 10:13,  2 users,  load average: 2.56, 2.71, 2.63 > unitime 1698334012617 > cd366eb6b8a4bf4beef64159fa803b61cd0a9a18

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -3,6 +3,7 @@
 /*
 Put the declarations of `push' instructions here.
 */
+make_instr_func(push_r_v);
 make_instr_func(push_rm_v);
 
 
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -23,11 +23,11 @@ static void instr_execute_1op()
 }
 
 //50-57   r->esp 16/32 push_rm_v
-make_instr_impl_1op(push,rm,v);       
+make_instr_impl_1op(push,r,v);       
 
 //FF /6 PUSH m16 5 Push memory word
 //FF /6 PUSH m32 5 Push memory dword  push_m_v
-//make_instr_impl_1op(push,m,v);    没有m，只有rm
+make_instr_impl_1op(push,rm,v);    //没有m，只有rm
 
 
 /*    
