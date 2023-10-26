cc789f3ad31174a74f5ca6ea94d5b06807904f85
tracer-pa-public <tracer@njuics.org>
2023-10-26 22:59:58 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:59:58 up 101 days,  9:46,  2 users,  load average: 2.88, 2.73, 2.60 > unitime 1698332398099 > 147ebdc9770194b5809e568d242bcf9bb4ef9af

--- nemu/include/cpu/instr/pop.h
+++ nemu/include/cpu/instr/pop.h
@@ -3,5 +3,6 @@
 /*
 Put the declarations of `pop' instructions here.
 */
+make_instr_func(pop_r_v);
 
 #endif
--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -2,3 +2,31 @@
 /*
 Put the implementations of `pop' instructions here.
 */
+
+//58 + rw POP r16 4 Pop top of stack into word register
+//58 + rd POP r32 4 Pop top of stack into dword register
+ 
+make_instr_func(pop_r_v)
+{
+    //print_asm_1("leave", "", 1, &rel);
+    opr_src.val = cpu.esp;
+    
+    //ebp pop
+    
+    //OPERAND m;
+    
+    
+    //m.data_size = data_size;
+    //m.type = OPR_MEM;
+    //m.addr = cpu.esp;
+    //m.val = eip;
+    //operand_read(&m);
+    //cpu.ebp = m.val;
+    
+    
+    cpu.esp += data_size/8;
+    //eip = m.val;
+    
+    
+    return 1;
+}
