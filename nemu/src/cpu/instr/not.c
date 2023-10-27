4007c75b33d4e0fe0c557f67f139643e0937521f
tracer-pa-public <tracer@njuics.org>
2023-10-27 15:00:50 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:00:50 up 102 days,  1:47,  2 users,  load average: 2.30, 2.51, 2.56 > unitime 1698390050505 > 8f1e2781889ef955f4a3963b47f9b2553ff03d9b

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -2,3 +2,19 @@
 /*
 Put the implementations of `not' instructions here.
 */
+
+static void instr_execute_1op() 
+{
+	operand_read(&opr_src);
+	
+	opr_src.val = ~opr_src.val,data_size
+	
+
+	operand_write(&opr_src);
+	
+}
+//f7   not_rm_v
+make_instr_impl_1op(not,rm,v);
+
+//f6
+make_instr_impl_1op(not,rm,b);
