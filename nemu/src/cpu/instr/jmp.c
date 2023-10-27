059359b2704a50e6473e44848d5ebe763cf31fb2
tracer-pa-public <tracer@njuics.org>
2023-10-27 19:46:53 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:46:53 up 102 days,  6:33,  2 users,  load average: 2.97, 2.73, 2.69 > unitime 1698407213306 > 162dea30f1c8d570ef56721c2bf6861ba0a4fd4e

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -56,7 +56,7 @@ make_instr_func(jmp_near_indirect)
 
         int offset = sign_ext(rm.val, data_size);
         
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+        print_asm_1("jmp", "", 1 + data_size / 8, &rm);
 
         cpu.eip += offset;
 
