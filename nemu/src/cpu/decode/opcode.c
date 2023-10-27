98848fc7d6166aa596056636d92833d1ca6109a1
tracer-pa-public <tracer@njuics.org>
2023-10-27 17:14:01 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:14:01 up 102 days,  4:00,  2 users,  load average: 3.61, 2.95, 2.76 > unitime 1698398040991 > 6bfd87ba749904dd26bc66018cd6d81ce185fbe

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -3,7 +3,7 @@
 instr_func opcode_entry[256] = {
     /* 0x00 - 0x03*/ inv, __ref_add_r2rm_v, add_rm2r_b, add_rm2r_v,//0
     /* 0x04 - 0x07*/ add_i2a_b, add_i2a_v, inv, inv,
-    /* 0x08 - 0x0b*/ or_r2rm_b, or_r2rm_v, or_rm2r_b, or_rm2r_v,
+    /* 0x08 - 0x0b*/ or_r2rm_b, __ref_or_r2rm_v, or_rm2r_b, or_rm2r_v,
     /* 0x0c - 0x0f*/ inv, inv, inv, opcode_2_byte,
     /* 0x10 - 0x13*/ inv, __ref_adc_r2rm_v, inv, inv,
     /* 0x14 - 0x17*/ inv, inv, inv, inv,
@@ -34,7 +34,7 @@ instr_func opcode_entry[256] = {
     /* 0x78 - 0x7b*/ js_short_, jns_short_, jp_short_, jnp_short_,
     /* 0x7c - 0x7f*/ jl_short_, jge_short_, jle_short_, jg_short_,//jle_short_,//jg_short_,
     /* 0x80 - 0x83*/ group_1_b, group_1_v, nemu_trap, group_1_bv,
-    /* 0x84 - 0x87*/ test_r2rm_b, test_r2rm_v, inv, inv,//85test_r2rm_v
+    /* 0x84 - 0x87*/ test_r2rm_b, __ref_test_r2rm_v, inv, inv,//85test_r2rm_v
     /* 0x88 - 0x8b*/ mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
     /* 0x8c - 0x8f*/ inv, lea_rm2r_v, inv, inv,//lea_rm2r_v
     /* 0x90 - 0x93*/ nop, inv, inv, inv,
