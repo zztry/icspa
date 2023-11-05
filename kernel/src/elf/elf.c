e3265211b6926645022c2a66d469f8ffb17cc3c9
tracer-pa-public <tracer@njuics.org>
2023-11-05 17:53:53 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:53:53 up 111 days,  4:40,  2 users,  load average: 1.37, 1.48, 1.42 > unitime 1699178033140 > 6899705794abb817a41c06bdd7a10c19727eb980

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,21 +42,16 @@ uint32_t loader()
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
             //从文件Offset开始位置，连续FileSiz个字节的内容需要被装载
-            for(uint32_t i = 0;i<ph->p_filesz;i++)
-            {
-                memcpy((ph->p_offset+i), (ph->p_vaddr+i), 8);
+            //for(uint32_t i = 0;i<ph->p_filesz;i++)
+            //{
+            //    memcpy((ph->p_offset+i), (ph->p_vaddr+i), 8);
                 //hw_mem[ph->p_vaddr+i] = hw_mem[ph->p_offset+i];
-            }
+            //}
             
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
             //装载到内存VirtAddr开始，连续MemSiz个字节的区域中 ，mem_sz - file_sz大小的位置为0
             memset(ph->p_vaddr+ph->p_filesz, 0, (ph->p_memsz - ph->p_filesz) );
-            /*for(uint32_t i = ph->p_filesz;i<ph->p_memsz;i++)
-            {
-                //memset(hw_mem, 0, MEM_SIZE_B);
-                memset(hw_mem+i, 0, 8);
-                //hw_mem[ph->p_vaddr+i] = 0;
-            }*/
+            
             
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
