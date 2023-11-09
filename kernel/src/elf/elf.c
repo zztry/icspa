ee5718fb789bb8387033eb6f35164a8c6d105bcd
tracer-pa-public <tracer@njuics.org>
2023-11-09 22:03:36 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:03:36 up 115 days,  8:50,  2 users,  load average: 2.11, 2.55, 2.52 > unitime > b90856134b3399042f2116238b49fc682f75ffa3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,7 +42,7 @@ uint32_t loader()
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
             //从文件Offset开始位置，连续FileSiz个字节的内容需要被装载
-            memcpy((void *)(ph->p_offset), (void *)(ph->p_vaddr), ph->p_filesz);
+            memcpy((void *)(ph->p_vaddr), (void *)(ph->p_offset), ph->p_filesz);  
             //for(uint32_t i = 0;i<ph->p_filesz;i++)
             //{
                 //hw_mem[ph->p_vaddr+i] = hw_mem[ph->p_offset+i];
