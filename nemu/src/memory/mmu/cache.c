743f18ce986567b0c6dfbbe7ede006d5c0d4e131
tracer-pa-public <tracer@njuics.org>
2023-11-22 16:23:05 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:23:05 up 128 days,  3:09,  2 users,  load average: 3.19, 2.90, 2.65 > unitime 1700641385184 > 45b46832e9cb28f1dbd06b8eed2ef53f0a3f476

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -141,6 +141,7 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	{
 	    memcpy(&ret,(void *)(hw_mem+paddr),len);
 	    //查看是否有空行
+	    /*
 	    if(pos!=-1)
 	    {
 	        //在第一个空行中写入
@@ -155,7 +156,7 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	        memcpy(caches[pos].data, (void *)(hw_mem+paddr-ingr_addr), 64); 
 	        caches[pos].valid_bit = true;
 			caches[pos].tag = tag_;
-	    }
+	    }*/
 	    
 	    
 	}
