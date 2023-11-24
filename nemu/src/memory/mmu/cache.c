eb1ee89ee31f8f87017091f4762c8509c0e1f0df
tracer-pa-public <tracer@njuics.org>
2023-11-24 10:45:07 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:45:07 up 129 days, 21:31,  2 users,  load average: 2.40, 2.40, 2.42 > unitime 1700793906977 > 5cc4348df93926b315384578dd93295d945209fc

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -113,7 +113,7 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	            is_match = true;
 	            if(len2==0)//不跨行
 	            {   
-	                /从后向前每次读取一个字节(从高位至低位)
+	                //从后向前每次读取一个字节(从高位至低位)
 	                for(int j = ingr_addr+len-1; j>=ingr_addr;j--)
 	                {
 	                    ret+=caches[i].data[j];
