cedc07c38435b3c72a07df03cf231a74147c3e63
tracer-pa-public <tracer@njuics.org>
2023-11-24 11:29:00 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:29:00 up 129 days, 22:15,  2 users,  load average: 2.77, 2.57, 2.57 > unitime > 1be81657a9b49e844b1a177862497872e21071f

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -17,6 +17,11 @@ void init_cache()
 	for(int i = 0;i<1024;i++)
 	{
 	    caches[i].valid_bit = false;
+	    caches[i].tag = 0;
+	    for(int j = 0;j<64;j++)
+	    {
+	        caches[i].data[j]=0;
+	    }
 	}
 	
 }
@@ -24,23 +29,7 @@ void init_cache()
 // write data to cache
 void cache_write(paddr_t paddr, size_t len, uint32_t data)
 {   
-    memcpy((void *)(hw_mem+paddr), &data, len);
-    
 	// implement me in PA 3-1
-	uint32_t tag_ = paddr>>13;      //标记
-	
-	
-	for(int i = 0;i<=1024;i++)
-	{
-	    if(caches[i].valid_bit==true&&caches[i].tag==tag_)//命中
-	    {
-	        caches[i].valid_bit = false;
-	        break;
-	    }
-	   
-	}
-	
-	
 	
 }
 
@@ -50,69 +39,7 @@ uint32_t cache_read(paddr_t paddr, size_t len)
     
 	// implement me in PA 3-1
 	
-	uint32_t ingr_addr = paddr & 0x3f;   //块内地址
-	uint32_t group = (paddr>>6)&0x7f;     //组号
-	uint32_t tag_ = paddr>>13;      //标记
-	
-	//组号对应从x1到x2行
-	uint32_t begin_line = group*8;
-	//uint32_t end_line = begin_line+7;
-	
-	//如果跨行/块 先分割长度
-	int len1 = len;
-	int len2 = 0;
-	if(64-ingr_addr<len)
-	{
-	    len1 = 64-ingr_addr;
-	    len2 = len-len1;
-	}
-	
-	
-	uint32_t ret;
-	
-	bool is_match = false;//是否命中
-	uint32_t pos = 0;//判断该组是否满,值为第一个无效的行
-	
-	
-	for(uint32_t i = begin_line;i<begin_line+8;i++)
-	{
-	    if(caches[i].valid_bit==true)
-	    {
-	        if(caches[i].tag==tag_)//命中，直接读取
-	        {
-	            is_match = true;
-	            if(len2==0)//不跨行
-	            {   
-	                
-	                memcpy(&ret,(void *)(&caches[i].data+ingr_addr),len);
-	                
-	            }
-	            else//跨行
-	            {
-	                memcpy(&ret,(void *)(hw_mem+paddr),len);
-	                
-	            }
-	            return ret;
-	        }
-	    }
-	}
-	
-	
-	//如果没有命中
-	if(is_match==false)
-	{
-	    memcpy(&ret,(void *)(hw_mem+paddr),len);
-	    
-	    pos = begin_line;
-	    memcpy((void *)caches[pos].data, (void *)(hw_mem+paddr-ingr_addr), 64); 
-	    caches[pos].valid_bit = true;
-		caches[pos].tag = tag_;
-		
-		
-	    
-	}
-	
-	
+	memcpy(&ret,(void *)(hw_mem+paddr),len);
 	
 	return ret;
 }
