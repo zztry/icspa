7805aaf6f0b36c039d1071838faae90b517ebd7d
tracer-pa-public <tracer@njuics.org>
2023-11-27 19:40:41 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:40:41 up 133 days,  6:27,  3 users,  load average: 2.27, 2.43, 2.48 > unitime 1701085241212 > d8a9208cd738c0782e9ad512d8b7c88880f680b7

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -39,7 +39,6 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
 	
 	//组号对应从x1到x2行
 	uint32_t begin_line = group*8;
-	uint32_t end_line = begin_line+7;
 	
 	
 	//如果跨行/块 先分割长度
@@ -50,20 +49,17 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
 	    len1 = 64-in_addr;
 	    len2 = len-len1;
 	}
+
 	
-	memcpy((void *)(hw_mem+paddr), &data, len);
-	
-	
-	
-	for(int i = begin_line;i<=end_line;i++)
+	for(int i = begin_line;i<begin_line+8;i++)
 	{
-	    if(caches[i].valid_bit==true&&caches[i].tag==tag_)//命中
+	    if(caches[i].valid_bit==true && caches[i].tag==tag_)//命中
 	    {
 	        caches[i].valid_bit = false;
 	        
 	        if(len2==0)//不跨行
 	        {
-	            memcpy((void *)(caches[i].data+in_addr), &data, len);
+	            memcpy((void *)(&caches[i].data[in_addr]), &data, len);
 				caches[i].tag = tag_;
 				caches[i].valid_bit = true;
 	        }
@@ -93,7 +89,6 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	
 	//组号对应从x1到x2行
 	uint32_t begin_line = group*8;
-	uint32_t end_line = begin_line+7;
 	
 	//如果跨行/块 先分割长度
 	int len1 = len;
@@ -111,7 +106,7 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	uint32_t pos = 0;//判断该组是否满,值为第一个无效的行
 	
 	
-	for(uint32_t i = begin_line;i<=end_line;i++)
+	for(uint32_t i = begin_line;i<begin_line+8;i++)
 	{
 	    if(caches[i].valid_bit==true)
 	    {
@@ -121,14 +116,14 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 	            if(len2==0)//不跨行
 	            {   
 	                
-	                memcpy(&ret,(void *)(&caches[i].data+in_addr),len);
+	                memcpy(&ret,(void *)(&caches[i].data[in_addr]),len);
 	                
 	            }
 	            else//跨行
 	            {
 	                
 	                //读取前半部分
-	                memcpy(&ret,(void *)(&caches[i].data+in_addr),len1);
+	                memcpy(&ret,(void *)(&caches[i].data[in_addr]),len1);
 	                //读取后半部分
 	                uint32_t ret2 = cache_read(paddr+len1,len2);//如果跨组/行都会在这里解决
 	                //后半部分为高位，左移
