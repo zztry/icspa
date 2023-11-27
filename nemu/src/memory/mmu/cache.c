ca726ce5745564f2fa9823e43e164341dbe60b56
tracer-pa-public <tracer@njuics.org>
2023-11-27 20:26:22 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:26:22 up 133 days,  7:12,  3 users,  load average: 2.61, 2.64, 2.63 > unitime 1701087982055 > e487180dfc5b391be22a29241183136f92dce794

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -103,13 +103,14 @@ uint32_t cache_read(paddr_t paddr, size_t len)
 			}
 			else//跨行
 			{
+			    uint32_t ret1;
 		    	//读取前半部分
-				memcpy(&ret, (void*)(&caches[group*8+i].data[in_addr]), len1);
+				memcpy(&ret1, (void*)(&caches[group*8+i].data[in_addr]), len1);
 				//读取后半部分
 				uint32_t ret2 = cache_read(paddr + len1, len2);//如果跨组/行都会在这里解决
 				//后半部分为高位，左移
 				ret2 = ret2 << (8 * len1);
-				ret = ret | ret2;
+				ret = ret1 | ret2;
 			}
 			/*
 			if(len2==0)
