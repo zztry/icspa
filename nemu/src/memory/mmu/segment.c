0bbe48fd8776f4831c8ecea3ce720dc10c7c2cef
tracer-pa-public <tracer@njuics.org>
2023-12-09 23:34:41 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:34:41 up 145 days, 10:21,  5 users,  load average: 2.77, 2.69, 2.56 > unitime 1702136081340 > a59a0f26c2067f4e390cc071eca95d143abf2b1

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -8,7 +8,7 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
 	
-	return cpu.segReg[sreg].base+offset;
+	return 0;//cpu.segReg[sreg].base+offset;
 }
 
 // load the invisible part of a segment register
@@ -19,9 +19,12 @@ void load_sreg(uint8_t sreg)
 	 */
 	 
 	 //对应段表地址,64位8字节
+	 /*
 	 uint32_t segt_addr = (uint32_t)hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8;
 	 SegDesc* sgdt = (SegDesc *)segt_addr;
+	 */
 	 //laddr_read  laddr_read(laddr_t laddr, size_t len)
+	 /*
 	 cpu.segReg[sreg].base = sgdt->base_15_0 + (sgdt->base_23_16<<16)+(sgdt->base_31_24<<24);
 	 cpu.segReg[sreg].limit = sgdt->limit_15_0+(sgdt->limit_19_16<<16);
 	 cpu.segReg[sreg].privilege_level = sgdt->privilege_level;
@@ -29,6 +32,7 @@ void load_sreg(uint8_t sreg)
 	 assert(cpu.segReg[sreg].limit == 0xfffff);
 	 assert(sgdt->present==1);  //?
 	 assert(sgdt->granularity==1);
+	 */
 	 
     
 }
