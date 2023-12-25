a7e84914059503da9082ca867526de194344b240
tracer-pa-public <tracer@njuics.org>
2023-12-25 22:07:18 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:07:18 up 161 days,  8:53,  5 users,  load average: 2.67, 2.61, 2.65 > unitime 1703513238557 > e9750f59872bdabaa4d8642f5507e9e115dda190

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -58,7 +58,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 		} 
 		else {
 			uint32_t paddr = page_translate(laddr);
-			return paddr_read(hwaddr, len);	
+			return paddr_read(paddr, len);	
 		}
 	}
 	else {
