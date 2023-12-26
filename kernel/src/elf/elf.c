333ec95dfe3a805895b22dfdf0d0cd62bcee864c
tracer-pa-public <tracer@njuics.org>
2023-12-26 17:08:18 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux 62be258773c1 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:08:18 up 162 days,  3:54,  5 users,  load average: 3.10, 3.13, 3.13 > unitime 1703581698225 > 4e3baf6c42e569b39c43dffe33c8af12cbb8eee

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -43,14 +43,14 @@ uint32_t loader()
             uint32_t p_paddr = 0;
 #ifdef IA32_PAGE
             p_paddr = mm_malloc(ph->p_vaddr,ph->p_memsz);
-            Log(" vaddr = %x  paddr = %x",ph->p_vaddr,p_paddr);
+            Log("vaddr = %x  paddr = %x",ph->p_vaddr,p_paddr);
             memcpy((void *)p_paddr, (void *)(ph->p_offset), ph->p_filesz); 
             if(ph->p_memsz>ph->p_filesz)
             {
                 memset((void *)(p_paddr+ph->p_filesz), 0, (ph->p_memsz - ph->p_filesz) );
             }
 #else
-            Log(" vaddr = %x",ph->p_vaddr);
+            Log("vaddr = %x",ph->p_vaddr);
             memcpy((void *)(ph->p_vaddr), (void *)(ph->p_offset), ph->p_filesz); 
             if(ph->p_memsz>ph->p_filesz)
             {
