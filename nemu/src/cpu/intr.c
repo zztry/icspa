d647f69c6c29218679a66fd812c7791bcd605d91
tracer-pa-public <tracer@njuics.org>
2024-01-23 22:24:59 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 22:24:59 up  3:55,  1 user,  load average: 0.00, 0.01, 0.00 > unitime 1706019899275 > b83e5c8966e5053b81fa1ac76445fb829df24c8

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,7 +11,36 @@ void raise_intr(uint8_t intr_no)
 
 
     // Push EFLAGS, CS, and EIP
+    OPERAND m;
+    m.type = OPR_MEM;
+    m.sreg = SREG_SS;
 
+    cpu.esp-=4;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    m.val = cpu.eflags.val;
+    operand_write(&m);
+
+    cpu.esp-=4;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    m.val = cpu.cs.val;
+    operand_write(&m);
+
+    cpu.esp-=4;
+    m.data_size = 32;
+    m.addr = cpu.esp;
+    m.val = cpu.eip;
+    operand_write(&m);
+
+    GateDesc* gate = (void *)(page_translate( segment_translate(cpu.idtr.base + intr_no*8 , SREG_CS) )+hw_mem)    
+
+    if(gate->type ==  INTERRUPT_GATE_32){
+        cpu.eflags.IF = 0; 
+    }
+
+    uint32_t offset = (gate->offset_31_16 << 16) + gate->offset_15_0;
+    cpu.eip = offset;
 #endif
 }
 
