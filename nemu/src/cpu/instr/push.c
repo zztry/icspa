8d52f27f795a5380da1cf75160c0dae6434bb593
tracer-pa-public <tracer@njuics.org>
2024-01-24 00:55:48 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 00:55:48 up  6:26,  1 user,  load average: 0.19, 0.44, 0.36 > unitime > eb3632b56d1227d2106e8a74af216d04dbcbceec

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -20,10 +20,10 @@ static void instr_execute_1op()
     opr_dest.type = OPR_MEM;
     opr_dest.addr = cpu.esp;
     opr_dest.val = opr_src.val;
-    //if(data_size==8)
-    //{
-    //    m.val = sign_ext(opr_src.val,opr_src.data_size);
-    //}
+    if(data_size==8)
+    {
+        opr_dest.val = sign_ext(opr_src.val,opr_src.data_size);
+    }
     operand_write(&opr_dest);
 }
 
