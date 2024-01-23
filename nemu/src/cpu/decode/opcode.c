4ce254b3af31bed861d1e3f8b445268e46b94bb6
tracer-pa-public <tracer@njuics.org>
2024-01-24 00:06:44 +0800

    > compile nemu testcase kernel > id 221220150 > user pa221220150 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 00:06:44 up  5:37,  1 user,  load average: 0.14, 0.05, 0.02 > unitime 1706026004309 > c5627eea8920724598ed3c959dae0f1894ef1520

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -23,7 +23,7 @@ instr_func opcode_entry[256] = {
     /* 0x4c - 0x4f*/ dec_r_v, dec_r_v, dec_r_v, dec_r_v,
     /* 0x50 - 0x53*/ push_r_v, push_r_v,  push_r_v,  push_r_v,
     /* 0x54 - 0x57*/ push_r_v, push_r_v,  push_r_v, push_r_v,
-    /* 0x58 - 0x5b*/ __ref_pop_r_v,  __ref_pop_r_v,  __ref_pop_r_v,  __ref_pop_r_v,
+    /* 0x58 - 0x5b*/ pop_r_v,  pop_r_v,  pop_r_v,  pop_r_v,
     /* 0x5c - 0x5f*/ __ref_pop_r_v, __ref_pop_r_v, __ref_pop_r_v, __ref_pop_r_v,
     /* 0x60 - 0x63*/ __ref_pusha, __ref_popa, inv, inv,
     /* 0x64 - 0x67*/ inv, inv, data_size_16, inv,
