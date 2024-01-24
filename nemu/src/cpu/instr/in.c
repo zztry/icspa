9c9d17a35227ff3af78101895967bec655b9a2bf
tracer-pa-public <tracer@njuics.org>
2024-01-25 03:53:51 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 03:53:51 up 14:49,  1 user,  load average: 0.12, 0.11, 0.06 > unitime > a50d9ce29490cdd3c8cedd5cfefdb55cbb6100d1

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -5,28 +5,28 @@ Put the implementations of `in' instructions here.
 */
 make_instr_func(in_b)
 {
-    OPERAND r;
+    //OPERAND r;
 
-    r.data_size = 8;
-    r.type = OPR_REG;
-    r.addr = 0x2;
-    operand_read(&r);
+    //r.data_size = 8;
+    //r.type = OPR_REG;
+    //r.addr = 0x2;
+    //operand_read(&r);
 
-    cpu.eax = pio_read(r.val,1);
+    cpu.eax = pio_read(cpu.edx,1);
 
     return 1;
 }
 
 make_instr_func(in_v)
 {
-    OPERAND r;
+    /*OPERAND r;
 
     r.data_size = data_size;
     r.type = OPR_REG;
     r.addr = 0x2;
-    operand_read(&r);
+    operand_read(&r);*/
 
-    cpu.eax = pio_read(r.val,data_size/8);
+    cpu.eax = pio_read(cpu.edx,data_size/8);
 
     return 1;
 }
