61186ff43a7cc78c2491527c0c06bad0a1a9c9fb
tracer-pa-public <tracer@njuics.org>
2024-01-25 03:44:15 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 03:44:15 up 14:39,  1 user,  load average: 0.22, 0.07, 0.02 > unitime > 69050ae96b5c361ca5a8cc0693228b095ece3715

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -3,3 +3,30 @@
 /*
 Put the implementations of `in' instructions here.
 */
+make_instr_func(in_b)
+{
+    OPERAND r;
+
+    r.data_size = 8;
+    r.type = OPR_REG;
+    r.addr = 0x2;
+    operand_read(&r);
+
+    cpu.eax = pio_read(r.val,1);
+
+    return 1;
+}
+
+make_instr_func(in_v)
+{
+    OPERAND r;
+
+    r.data_size = data_size;
+    r.type = OPR_REG;
+    r.addr = 0x2;
+    operand_read(&r);
+
+    cpu.eax = pio_read(r.val,data_size/8);
+
+    return 1;
+}
--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -3,3 +3,17 @@
 /*
 Put the implementations of `out' instructions here.
 */
+make_instr_func(out_b)
+{
+    
+    pio_write(cpu.edx,1,cpu.eax);
+
+    return 1;
+}
+
+make_instr_func(out_v)
+{
+    
+    pio_write(cpu.edx,data_size/8,cpu.eax);
+    return 1;
+}
