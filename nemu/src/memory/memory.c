90ac6babbad33406b54ee79d8de9728e38a21145
tracer-pa-public <tracer@njuics.org>
2024-01-28 00:04:11 +0800

    > clean > id 221220150 > user pa221220150 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 00:04:11 up 21 min,  1 user,  load average: 0.00, 0.16, 0.39 > unitime > c909fbeba76a73d55b3d452790a51e7811a963ca

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -95,16 +95,16 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data)
         //printf("cpu.cr0.pg=1 now.  laddr_write  \n");
         //fflush(stdout);
         if ((laddr>>12) !=((laddr+len-1)>>12)) {
-            printf("data= %x \n",data);
-            fflush(stdout);
+            //printf("data= %x \n",data);
+            //fflush(stdout);
 		    uint32_t len1 = (((laddr>>12)+1)<<12)-laddr;
 		    uint32_t len2 = len - len1;
 		    uint32_t addr1 = page_translate(laddr);  //低位
 		    uint32_t addr2 = page_translate( ((laddr>>12)+1)<<12 );
 		    uint32_t data1 = data - ((data>>(8*len1))<<(8*len1));
 		    uint32_t data2 = (data>>(8*len1));
-		    printf("data1= %x data2 = %x \n",data1,data2);
-            fflush(stdout);
+		    //printf("data1= %x data2 = %x \n",data1,data2);
+            //fflush(stdout);
 		    paddr_write(addr1,len1,data1);
 		    paddr_write(addr2,len2,data2);
 		} 
